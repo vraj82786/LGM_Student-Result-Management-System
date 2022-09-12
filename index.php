de06c328285c2be4c00ae6abf5a8f621475626e6
Varun Indrajit Anand <87412734+vraj82786@users.noreply.github.com>
2022-09-12 11:07:56 +0530

    Update index.php
    Added some content

--- index.php
+++ index.php
@@ -22,5 +22,19 @@
             </ul>
         </div>
     </div>
+    <div>
+        "The Governor as Chancellor plays a very constructive role in the administration of the Universities. The Chancellor being the Governor can help to insulate the Universities from unnecessary governmental interference and safeguard their autonomy."
+More>>
+
+Shri. Bhagat Singh Koshyari
+Chancellor
+    </div>
+    <div>
+        The Savitribai Phule Pune University has been diligently pursuing the mission of the University – Conservation, Creation, Advancement and Dissemination of knowledge through its 18 schools consisting of 58 departments and centres. Referred to as the ‘Oxford of the East’ the University has been
+More>>
+
+Prof.(Dr.) Karbhari Vishwanath Kale
+Vice Chancellor
+    </div>
 </body>
 </html>
